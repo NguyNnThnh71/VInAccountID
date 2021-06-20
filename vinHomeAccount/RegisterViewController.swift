d29e08bda9fabf3edf3f3663241b3efc31988100
NguyNnThnh71 <nguyenducthanh7778@gmail.com>
2021-06-20 13:55:44 +0700

    Register

---
+++ vinHomeAccount/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  vinHomeAccount
+//
+//  Created by Apple on 20/06/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var NameUI: UITextField!
+    @IBOutlet weak var phoneNumberUI: UITextField!
+    @IBOutlet weak var BackButton: UIButton!
+    @IBOutlet weak var RegisterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NameUI.placeholder = " Họ và tên"
+        phoneNumberUI.placeholder = "Số điện thoại "
+        NameUI.keyboardType = .namePhonePad
+        phoneNumberUI.keyboardType = .numberPad
+        NameUI.borderStyle = .roundedRect
+        phoneNumberUI.borderStyle = .roundedRect
+        BackButton.backgroundColor = UIColor(red: 0.00, green: 0.45, blue: 0.38, alpha: 1.00)
+        RegisterButton.backgroundColor = UIColor(red: 0.00, green: 0.45, blue: 0.38, alpha: 1.00)
+    }
+    @IBAction func RegisterPress(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let LoginVC = storyboard.instantiateViewController(identifier: "LoginViewController") as LoginViewController
+        
+        LoginVC.modalPresentationStyle = .fullScreen
+        
+        
+        self.present(LoginVC, animated: true, completion: nil)
+    }
+    
+}
